8df1e79f5bf115e09008f202c3306ad4cbee3a00
sooxuu <salajang21@gmail.com>
2023-07-24 02:44:28 +0900

    update

--- src/components/PlaceList.js
+++ src/components/PlaceList.js
@@ -1,3 +1,16 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const [place, setPlace] = useState([]);
+const getPlace = () => {
+    axios.get('https://jsonplaceholder.typicode.com/posts').then((res) => {
+        console.log(res);
+    })
+};
+
+useEffect(() => {
+    getPlace();
+}, []);
 
 const PlaceList = () => {
     return (
